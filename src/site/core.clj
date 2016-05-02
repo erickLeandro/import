20cc487ea6dddecec6fa7d6c14d90331b347c2bf
erick leandro <erick.leandro.lima@hotmail.com>
2016-05-02 15:53:02 -0400

    bug on java reader

--- src/site/core.clj
+++ src/site/core.clj
@@ -31,7 +31,7 @@
        (re-find #"\d")))
 
 (defn- find-in-fs [old-news-entry]
-  (let [id              (:id old-news-entry)
+  (let [id              (:old-id old-news-entry)
         kws             (:file-kws old-news-entry)
         possible-fnames (map (fn [kw]
                                (str news-path "/" (file-id kw) "/" id ".jpg"))
@@ -39,21 +39,23 @@
         existing-files  (filter #(.exists %) (map io/file possible-fnames))]
     existing-files))
 
-(defn with-files [old-news]
+(defn with-files [entries]
   (map
-   (fn [old-news-entry]
-     (let [valid-kws (valid-files old-news-entry)]
-       (-> (assoc old-news-entry :file-kws valid-kws)
-           (find-in-fs))))
-   old-news))
-
-#_(-> (j/query
-     db-source
-     (-> (select :*)
-         (from :mac_noticias)
-         (where [:= :id 806])
-         (sql/format)))
-    (with-files))
+   (fn [entry]
+     (let [valid-kws (valid-files entry)
+          files (-> (assoc entry :file-kws valid-kws)
+                    (find-in-fs))]
+      (-> entry
+        (dissoc :old-id 
+                :foto1 
+                :foto2 
+                :foto3 
+                :foto4
+                :foto5
+                :foto6
+                :file-kws)
+        (assoc :files files))))
+   entries))
 
 ;; TODO
 ;; 1) Gerar um map de Noticias Antigas -> Noticias Novas
@@ -61,10 +63,30 @@
 ;; 3) Mover os arquivos existens para o Storage
 ;; 4) Criar o registro no banco de dados na tabela _upload
 
-(defn new-entry [{:keys [titulo texto data]}]
+(defn new-entry [{:keys [id titulo texto data foto1 foto2 foto3 foto4 foto5 foto6]}]
   {:noticia_titulo   titulo
+   :organogramaCod 5
+   :noticia_tipo "P"
    :noticia_conteudo texto
-   :noticia_data     data})
+   :noticia_data     data
+   :noticia_privacidade "P"
+   :noticia_comentario "N"
+   :old-id id
+   :foto1 foto1
+   :foto2 foto2
+   :foto3 foto3
+   :foto4 foto4
+   :foto5 foto5
+   :foto6 foto6})
 
-(defn persist-entry [db-target entry]
-  (j/insert-multi! db-target :noticia entry))
+(defn persist-entry [db-target entries]
+  (j/insert-multi! db-target :noticia entries))
+
+(->> (j/query
+     db-source
+     (-> (select :*)
+         (from :mac_noticias)
+         (where [:= :id 806])
+         (sql/format)))
+    (map new-entry)
+    (with-files))
