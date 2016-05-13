a191d985d8e4c2b646dd6e08a5ac4bcdd5952d31
erick leandro <erick.leandro.lima@hotmail.com>
2016-05-13 14:24:39 -0400

    finish him

--- src/duplicates/core.clj
+++ src/duplicates/core.clj
@@ -1,73 +1,53 @@
 (ns duplicates.core
   (:require [clojure.java.jdbc :as j]
             [honeysql.core :as sql]
-            [honeysql.helpers :refer [select from where group having order-by join delete-from]]
+            [honeysql.helpers :refer [select from where group having order-by join delete-from left-join]]
             [clojure.java.io :as io]
             [environ.core :refer [env]]))
 
 ;; =============================================================================
 ;; Config vars
 ;; =============================================================================
-(def organograma 4)
+(def organograma 3)
 
 ;; =============================================================================
 ;; DB Setup
 ;; =============================================================================
 
 (def db-target {:subprotocol "mysql"
-                :subname "//localhost:3306/potencia?zeroDateTimeBehavior=convertToNull"
-                :user "root"
-                :password "q1w2e3"})
+                :subname "//s2.virtuaserver.com.br:3306/centersi_test?zeroDateTimeBehavior=convertToNull"
+                :user "centersi_01"
+                :password "J6foEAiP!{1^"})
 
 (defn get-invalid-entries []
   (j/query
    db-target
-   (-> (select :usuarioCod :usuarioLogin :numeroAcessos)
-       (from :_usuario)
+   (-> (select :u.usuarioCod)
+       (from [:_usuario :u])
+       (left-join [:pessoa :p] [:= :u.usuarioCod :p.usuarioCod])
        (where [:in :usuarioLogin (-> (select :usuarioLogin)
-                                     (from :_usuario)
-                                     (where [:= :organogramaCod organograma])
-                                     (group :usuarioLogin)
+                                     (from [:_usuario :u2])
+                                     (where [:= :u2.organogramaCod organograma])
+                                     (group :u2.usuarioLogin)
                                      (having [:> :%count.* 1]))]
-              [:= :organogramaCod organograma])
+              [:= :u.organogramaCod organograma]
+              [:= :p.usuarioCod nil])
+        (order-by :usuarioLogin)
        (sql/format))))
 
-(defn separate-by-login [rs]
-  (reduce (fn [separated user]
-            (update-in separated [(:usuariologin user)] #(conj % user)))
-          {}
-          rs))
-
-(defn duplicates [separated]
-  (reduce (fn [to-exclude [_ users]]
-            (let [ordered (sort (fn [a b]
-                                  (cond
-                                    (> (:numeroacessos a) (:numeroacessos b)) -1
-                                    (< (:numeroacessos a) (:numeroacessos b)) 1
-                                    :else 0))
-                                users)]
-              (if (seq (rest ordered))
-                (conj to-exclude (first (rest ordered)))
-                to-exclude)))
-          []
-          separated))
-
-(defn remove-user [user]
-  (println "Removing user: " user)
-  (let [id (:usuariocod user)]
+(defn remove-user [id]
+  (println "Removing user login: " id)
     (j/with-db-transaction [conn db-target]
       (j/delete! db-target :_log ["usuarioCod = ?" id])
       (j/delete! db-target :_usuario_recovery ["usuarioCod = ?" id])
-      (j/delete! db-target :_usuario ["usuarioCod = ?" id]))))
+      (j/delete! db-target :_usuario ["usuarioCod = ?" id])))
 
-(defn check-invalids []
+(defn check-invalids! []
   (println "Start check duplicates")
-  (let [dups (-> (get-invalid-entries)
-                 (separate-by-login)
-                 (duplicates))]
+  (let [ids (get-invalid-entries)]
     (doall
-     (map remove-user dups))))
+     (map (fn [x] (remove-user (:usuariocod x))) ids))))
 
 (defn -main [& args]
-  (check-invalids)
+  (check-invalids!)
   (println "Done!"))
